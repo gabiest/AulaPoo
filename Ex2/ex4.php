38f6c2b621d92c3f819c9f8212b3d305e69ab094
Gabrielly Vieira dos Santos <50851101+gabiest@users.noreply.github.com>
2022-11-10 19:28:36 -0300

    Ex2

---
+++ Ex2/ex4.php
@@ -0,0 +1,47 @@
+<?php
+
+if( isset($_GET['perimetro']) ){
+  $perimetro = $_GET['perimetro'];
+
+  $diametro = getDiametro($perimetro);
+  $raio     = getRaio($diametro);
+  $area     = getArea($raio);
+}
+
+
+function getDiametro($p){
+  return $p / 3.14;
+}
+
+function getRaio($d){
+  return $d / 2;
+}
+
+function getArea($r){
+  return $r * $r * 3.14;
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="ex4.php"method="GET" >
+      <label>Digite o Perimetro:</label>
+      <input type="number" name="perimetro">
+
+      <input type="submit" value="Calcular">
+
+      <?php echo "Diametro:{$diametro} &nbsp; &nbsp; &nbsp; Raio:{$raio} &nbsp; &nbsp; &nbsp; Area:{$area} "; 
+      ?>
+    </form>
+
+
+
+   
+  </body>
+</html>
